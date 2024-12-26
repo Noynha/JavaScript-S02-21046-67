fba2662abaf379b697ea200a272ff0f41993871f
Noynha <paritasurin@gmail.com>
2024-12-26 10:24:40 +0700

    Lecture3

---
+++ Lecture03/arrayReducer.js
@@ -0,0 +1,11 @@
+const array = [15,16,17,18,19];
+
+function reduce(accumlator,currentValue,index) {
+    const returns = accumlator + currentValue;
+    console.log(
+        `accumulator: ${accumlator},currntValue: ${currentValue},index: ${index},returns: ${returns}`
+    );
+    return returns;
+}
+
+array.reduce(reduce);
